ed9998dcece437ca7fccab2cb4bba4e19a0a08f4
sentomo <tomoko.senju@jrits.ricoh.co.jp>
2024-10-25 00:54:08 +0900

    add: 研修時のメモを追記

--- exercises/ch14/ex01/index.js
+++ exercises/ch14/ex01/index.js
@@ -25,13 +25,13 @@ export function unwritableAndUnconfigurableObj() {
   return obj;
 }
 
-// 書き込み可能、再定義不可のオブジェクトを返す関数
+// 書き込み可能、再定義不可のオブジェクトを返す関数→sealを使って、オブジェクト封印+プロパティ削除を禁止することもできる
 export function writableAndUnconfigurableObj() {
   const obj = {};
   Object.defineProperty(obj, 'b', {
     value: 2,
     writable: true,
-    configurable: false,
+    configurable: false, // 不要？
     enumerable: true
   });
   return obj;
