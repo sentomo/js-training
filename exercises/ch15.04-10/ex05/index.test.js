02b61b5a4931bdcc67d688360bf21c75fdbf9ab5
sentomo <tomoko.senju@jrits.ricoh.co.jp>
2024-11-07 11:06:28 +0900

    fix test cord

--- exercises/ch15.04-10/ex05/index.test.js
+++ exercises/ch15.04-10/ex05/index.test.js
@@ -13,20 +13,18 @@ test.describe('Inline Circle', () => {
   });
   
   test('inline-circleに正しい属性が設定されている', async ({ page }) => {
-    await page.waitForSelector('inline-circle'); // 要素が表示されるのを待つ
     const inlineCircles = await page.locator('inline-circle');
-    console.log(`inlineCircles: ${inlineCircles.first()}`);
 
     // デフォルトのinline-circleの属性確認
-    await expect(circles.nth(0)).toHaveAttribute('diameter', '1em');
-    await expect(circles.nth(0)).not.toHaveAttribute('color', 'blue');
+    const borderColor = await inlineCircles.first().evaluate((element) => getComputedStyle(element).borderColor);
+    expect(borderColor).toBe("rgb(144, 238, 144)");  // Playwriteでは色の名前で比較はできない
 
     // 2番目のinline-circleの属性確認
-    await expect(circles.nth(1)).toHaveAttribute('diameter', '1.2em');
-    await expect(circles.nth(1)).toHaveAttribute('color', 'blue');
+    await expect(inlineCircles.nth(1)).toHaveAttribute('diameter', '1.2em');
+    await expect(inlineCircles.nth(1)).toHaveAttribute('color', 'blue');
 
     // 3番目のinline-circleの属性確認
-    await expect(circles.nth(2)).toHaveAttribute('diameter', '.6em');
-    await expect(circles.nth(2)).toHaveAttribute('color', 'gold');
+    await expect(inlineCircles.nth(2)).toHaveAttribute('diameter', '.6em');
+    await expect(inlineCircles.nth(2)).toHaveAttribute('color', 'gold');
   });
 });
