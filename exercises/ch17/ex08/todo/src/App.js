60ddf30535ee7f5768cd8b6b089baff717f87b6e
sentomo <tomoko.senju@jrits.ricoh.co.jp>
2025-02-06 04:27:11 +0900

    Add ch17 answers

--- exercises/ch17/ex08/todo/src/App.js
+++ exercises/ch17/ex08/todo/src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]); // todosはToDoリストを配列として管理、setTodosはtodosを更新する関数
+  const [input, setInput] = useState(''); // inputは入力値を文字列として管理、setInputはinputを更新する関数
+
+  const addTodo = (event) => {
+    event.preventDefault(); // フォーム送信のリロードを防ぐ
+    if (input.trim() === '') return;
+    setTodos([{ text: input.trim(), completed: false }, ...todos]); // 既存のリストに新しいToDoオブジェクトを追加
+    setInput(''); // テキストボックスを空白にする
+  };
+
+  const toggleTodo = (index) => {
+    setTodos(
+      todos.map((todo, i) =>
+        i === index ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index)); // array.filterの第一引数は配列のelementが来るが、使用しないので_を使う
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <form onSubmit={addTodo}>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="What needs to be done?"
+            />
+            <button type="submit">Add</button>
+          </form>
+          <ul>
+            {todos.map((todo, index) => (
+              <li key={index}>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => toggleTodo(index)}
+                />
+                <label
+                  style={{
+                    textDecoration: todo.completed ? 'line-through' : 'none',
+                  }}
+                >
+                  {todo.text}
+                </label>
+                <button onClick={() => deleteTodo(index)}>❌</button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
